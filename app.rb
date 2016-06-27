fd9473de4e34d0023305b6fca81b47601c00695e
Alex Soble <asoble@gmail.com>
2016-06-27 17:29:05 -0500

    Revert "Use gem to enable cross-origin requests"

--- app.rb
+++ app.rb
@@ -1,11 +1,6 @@
 require          "sinatra"
-require          "sinatra/cross_origin"
 require          "json"
 require_relative "documents_api"
-
-configure do
-  enable :cross_origin
-end
 
 get '/api/:benefits_application' do
   content_type :json
