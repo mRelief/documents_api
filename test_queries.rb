e5ffd39a33779f0c68e66589be5d6224a9a9ba1e
Alex Soble <asoble@gmail.com>
2016-06-27 15:01:12 -0500

    Add local test queries

--- test_queries.rb
+++ test_queries.rb
@@ -1,4 +1,30 @@
 require_relative "test_fixtures"
 require          "active_support/all"
 
-puts "localhost:4567/api/#{SINGLE_HOUSEHOLD_MEMBER_EMPLOYED.to_query}"
+puts; puts "Single employed household member:"
+
+puts "http://localhost:4567/api/#{SINGLE_HOUSEHOLD_MEMBER_EMPLOYED.to_query}"
+
+puts; puts "Single self employed household member:"
+
+puts "http://localhost:4567/api/#{SINGLE_HOUSEHOLD_MEMBER_SELF_EMPLOYED.to_query}"
+
+puts; puts "Single-member household with rental income:"
+
+puts "http://localhost:4567/api/#{SINGLE_HOUSEHOLD_MEMBER_WITH_RENTAL_INCOME.to_query}"
+
+puts; puts "Multi-member household, head of household recieving child support:"
+
+puts "http://localhost:4567/api/#{MULTI_MEMBER_HOUSEHOLD_RECEIVING_CHILD_SUPPORT.to_query}"
+
+puts; puts "Household applying for expedited benefits:"
+
+puts "http://localhost:4567/api/#{EXPEDITED_BENEFITS.to_query}"
+
+puts; puts "Multi-member household -- retiree, disabled person, working person:"
+
+puts "http://localhost:4567/api/#{MULTI_MEMBER_HOUSEHOLD_WITH_RETIREE_DISABLED_AND_WORKING.to_query}"
+
+puts; puts "Multi-member household -- unemployed person, working person:"
+
+puts "http://localhost:4567/api/#{MULTI_MEMBER_HOUSEHOLD_WITH_UNEMPLOYED_AND_WORKING.to_query}"
