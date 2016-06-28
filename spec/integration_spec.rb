20990b35f957ae869e33dd41f959290e17f44dc0
Alex Soble <asoble@gmail.com>
2016-06-28 16:33:29 -0500

    Skeleton integration test

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,7 +1,16 @@
+require 'rack/test'
+
+require_relative "spec_helper"
 require_relative "test_queries"
+require_relative "../app"
 
 describe "queries against API endpoints" do
   it 'does the right thing' do
-    puts TEST_QUERIES
+    get '/'
+    # Rspec 2.x
+    expect(last_response).to be_ok
+
+    # Rspec 1.x
+    last_response.should be_ok
   end
 end
