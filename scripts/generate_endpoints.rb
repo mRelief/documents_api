f0358b4f7ba7d718bf65a78d095902b7712951c1
Alex Soble <asoble@gmail.com>
2016-07-05 14:49:14 -0500

    Update script for endpoints to encode requests in JSON

--- scripts/generate_endpoints.rb
+++ scripts/generate_endpoints.rb
@@ -1,10 +1,16 @@
 require_relative "../spec/test_queries"
 require          "active_support/all"
+require          "uri"
 
 @base_url = ARGV[0]
 
+
 def url_endpoint(base_url, query_data)
-  "http://#{base_url}/api/#{query_data.to_query}"
+  json_data = JSON.generate(query_data)
+
+  query_string = URI.encode(json_data)
+
+  return "http://#{base_url}/api/#{query_string}"
 end
 
 TEST_QUERIES.each do |query|
