6fd022e7d925a8c487c3cde6c94f1d3c33d67490
Alex Soble <asoble@gmail.com>
2016-07-18 15:54:31 -0500

    Add new required fields and dry up test fixtures

--- spec/test_fixtures.rb
+++ spec/test_fixtures.rb
@@ -1,153 +1,68 @@
 ## Fixtures for specs:
 
-SINGLE_HOUSEHOLD_MEMBER_EMPLOYED = {
-  household_members: [
-    {
-      "child_under_18": "false",
-      "disability_benefits": "false",
-      "is_employee": "true",
-      "self_employed": "false",
-      "receiving_child_support": "false",
-      "is_retired": "false",
-      "receiving_unemployment_benefits": "false",
-    }
-  ],
-  is_applying_for_expedited: "false"
+BASELINE_HOUSEHOLD_MEMBER = {
+  child_under_18: "false",
+  disability_benefits: "false",
+  is_employee: "false",
+  self_employed: "false",
+  receiving_child_support: "false",
+  is_retired: "false",
+  receiving_unemployment_benefits: "false",
 }
 
-SINGLE_HOUSEHOLD_MEMBER_SELF_EMPLOYED = {
-  household_members: [
-    {
-      "child_under_18": "false",
-      "disability_benefits": "false",
-      "is_employee": "false",
-      "self_employed": "true",
-      "receiving_child_support": "false",
-      "is_retired": "false",
-      "receiving_unemployment_benefits": "false",
-    }
-  ],
-  is_applying_for_expedited: "false"
-}
+CHILD = BASELINE_HOUSEHOLD_MEMBER.clone
+CHILD[:child_under_18] = "true"
 
-SINGLE_HOUSEHOLD_MEMBER_WITH_RENTAL_INCOME = {
-  household_members: [
-    {
-      "child_under_18": "false",
-      "disability_benefits": "false",
-      "is_employee": "false",
-      "self_employed": "true",
-      "receiving_child_support": "false",
-      "is_retired": "false",
-      "receiving_unemployment_benefits": "false",
-    }
-  ],
+BASELINE = {
+  household_members: [BASELINE_HOUSEHOLD_MEMBER],
   is_applying_for_expedited: "false",
-  has_rental_income: "true"
+  has_rental_income: "false",
+  renting: "false",
+  owns_home: "false",
+  shelter: "false",
+  has_no_income: "true",
+  living_with_family_or_friends: "false",
+  all_citizens: "true"
 }
 
-MULTI_MEMBER_HOUSEHOLD_RECEIVING_CHILD_SUPPORT = {
-  household_members: [
-    {
-      "child_under_18": "false",
-      "disability_benefits": "false",
-      "is_employee": "true",
-      "self_employed": "false",
-      "receiving_child_support": "true",
-      "is_retired": "false",
-      "receiving_unemployment_benefits": "false",
-    },
-    {
-      "child_under_18": "true",
-      "disability_benefits": "false",
-      "is_employee": "false",
-      "self_employed": "false",
-      "receiving_child_support": "false",
-      "is_retired": "false",
-      "receiving_unemployment_benefits": "false",
-    },
-    {
-      "child_under_18": "true",
-      "disability_benefits": "false",
-      "is_employee": "false",
-      "self_employed": "false",
-      "receiving_child_support": "false",
-      "is_retired": "false",
-      "receiving_unemployment_benefits": "false",
-    },
-  ],
-  is_applying_for_expedited: "false"
-}
+EMPLOYED_PERSON = BASELINE_HOUSEHOLD_MEMBER.clone
+EMPLOYED_PERSON[:is_employee] = "true"
 
-EXPEDITED_BENEFITS = {
-  household_members: [
-    {
-      "child_under_18": "false",
-      "disability_benefits": "false",
-      "is_employee": "false",
-      "self_employed": "true",
-      "receiving_child_support": "false",
-      "is_retired": "false",
-      "receiving_unemployment_benefits": "false",
-    }
-  ],
-  is_applying_for_expedited: "true"
-}
+SINGLE_HOUSEHOLD_MEMBER_EMPLOYED = BASELINE.clone
+SINGLE_HOUSEHOLD_MEMBER_EMPLOYED[:household_members] = [EMPLOYED_PERSON]
 
-MULTI_MEMBER_HOUSEHOLD_WITH_RETIREE_DISABLED_AND_WORKING = {
-  household_members: [
-    {
-      "child_under_18": "false",
-      "disability_benefits": "false",
-      "is_employee": "false",
-      "self_employed": "false",
-      "receiving_child_support": "false",
-      "is_retired": "true",
-      "receiving_unemployment_benefits": "false",
-    },
-    {
-      "child_under_18": "false",
-      "disability_benefits": "true",
-      "is_employee": "false",
-      "self_employed": "false",
-      "receiving_child_support": "false",
-      "is_retired": "false",
-      "receiving_unemployment_benefits": "false",
-    },
-    {
-      "child_under_18": "false",
-      "disability_benefits": "false",
-      "is_employee": "true",
-      "self_employed": "false",
-      "receiving_child_support": "false",
-      "is_retired": "false",
-      "receiving_unemployment_benefits": "false",
-    },
-  ],
-  is_applying_for_expedited: "false"
-}
+SINGLE_HOUSEHOLD_MEMBER_SELF_EMPLOYED = BASELINE.clone
+SINGLE_HOUSEHOLD_MEMBER_SELF_EMPLOYED[:household_members][0][:self_employed] = true
 
+SINGLE_HOUSEHOLD_MEMBER_WITH_RENTAL_INCOME = BASELINE.clone
+SINGLE_HOUSEHOLD_MEMBER_WITH_RENTAL_INCOME[:has_rental_income] = "true"
 
-MULTI_MEMBER_HOUSEHOLD_WITH_UNEMPLOYED_AND_WORKING = {
-  household_members: [
-    {
-      "child_under_18": "false",
-      "disability_benefits": "false",
-      "is_employee": "false",
-      "self_employed": "false",
-      "receiving_child_support": "false",
-      "is_retired": "false",
-      "receiving_unemployment_benefits": "true",
-    },
-    {
-      "child_under_18": "false",
-      "disability_benefits": "false",
-      "is_employee": "true",
-      "self_employed": "false",
-      "receiving_child_support": "false",
-      "is_retired": "false",
-      "receiving_unemployment_benefits": "false",
-    },
-  ],
-  is_applying_for_expedited: "false"
-}
+PERSON_RECEIVING_CHILD_SUPPORT = BASELINE_HOUSEHOLD_MEMBER
+PERSON_RECEIVING_CHILD_SUPPORT[:receiving_child_support] = "true"
+
+MULTI_MEMBER_HOUSEHOLD_RECEIVING_CHILD_SUPPORT = BASELINE.clone
+MULTI_MEMBER_HOUSEHOLD_RECEIVING_CHILD_SUPPORT[:household_members] = [
+  PERSON_RECEIVING_CHILD_SUPPORT, CHILD, CHILD
+]
+
+EXPEDITED_BENEFITS = BASELINE.clone
+EXPEDITED_BENEFITS[:is_applying_for_expedited] = "true"
+
+RETIREE = BASELINE_HOUSEHOLD_MEMBER.clone
+RETIREE[:is_retired] = "true"
+
+DISABLED_PERSON = BASELINE_HOUSEHOLD_MEMBER.clone
+DISABLED_PERSON[:disability_benefits] = "true"
+
+MULTI_MEMBER_HOUSEHOLD_WITH_RETIREE_DISABLED_AND_WORKING = BASELINE.clone
+MULTI_MEMBER_HOUSEHOLD_WITH_RETIREE_DISABLED_AND_WORKING[:household_members] = [
+  RETIREE, DISABLED_PERSON, SINGLE_HOUSEHOLD_MEMBER_EMPLOYED
+]
+
+UNEMPLOYED_PERSON = BASELINE_HOUSEHOLD_MEMBER.clone
+UNEMPLOYED_PERSON[:receiving_unemployment_benefits] = "true"
+
+MULTI_MEMBER_HOUSEHOLD_WITH_UNEMPLOYED_AND_WORKING = BASELINE.clone
+MULTI_MEMBER_HOUSEHOLD_WITH_UNEMPLOYED_AND_WORKING[:household_members] = [
+  UNEMPLOYED_PERSON, EMPLOYED_PERSON
+]
