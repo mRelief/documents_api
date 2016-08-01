8827db01d04bae57014bf500e2ea2f8235325a40
Alex Soble <asoble@gmail.com>
2016-08-01 15:36:11 -0500

    Stop mucking around with JSON, we need Rails activesupport to_query

--- lib/endpoint_generator.rb
+++ lib/endpoint_generator.rb
@@ -4,11 +4,7 @@ require "json"
 class EndpointGenerator < Struct.new :base_url, :query_data
 
   def generate_url
-    json_data = JSON.generate(query_data)
-
-    query_string = URI.encode(json_data)
-
-    return "http://#{base_url}/api/#{query_string}"
+    return "http://#{base_url}/api/#{query_data.to_query}"
   end
 
 end
