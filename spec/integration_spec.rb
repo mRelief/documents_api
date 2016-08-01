71abc78236d7e4f6bee134a5e24590ae1bfcc2d8
Alex Soble <asoble@gmail.com>
2016-08-01 15:38:03 -0500

    Unskip these specs and delete the whining

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -14,19 +14,11 @@ describe "queries against API endpoints" do
     url = EndpointGenerator.new(nil, query[:ruby_hash]).generate_url
 
     it "doesn't fail horribly" do
-      skip "rspec and Rack URI do not play well with these endpoints" do
-        get url, format: :json
+      get url, format: :json
 
-        # ^^ Having trouble getting rspec request spec to play nicely with
-        # JSON-encoded URL query strings, throws `URI::InvalidURIError: bad URI(is not URI?)`
-        # These query strings work fine in the browser..
-        # Need to figure out why rspec/URI don't like 'em
-
-        expect(last_response).to be_ok
-        expect(response_json.keys).to eq [ "household_members", "other_documents_needed" ]
-      end
+      expect(last_response).to be_ok
+      expect(response_json.keys).to eq [ "household_members", "other_documents_needed" ]
     end
-
   end
 
 end
