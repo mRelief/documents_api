fc5952d04db1dcbdc1ecf4549c7668f712fc5950
Alex Soble <asoble@gmail.com>
2016-08-03 15:02:32 -0500

    Make minimal integration test
    + #101

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -8,14 +8,34 @@ describe "queries against API endpoints" do
 
   let(:response_json) { JSON.parse(last_response.body) }
 
-  skip do
-    path = "/api?#{query[:ruby_hash].to_query}"
-    it "doesn't fail horribly" do
-      get path, format: :json
+  let(:default_params) {
+    {
+      disability_benefits: "false",
+      employee: "false",
+      self_employed: "false",
+      child_support: "false",
+      retired: "false",
+      unemployment_benefits: "false",
+      has_rental_income: "false",
+      renting: "false",
+      owns_home: "false",
+      shelter: "false",
+      living_with_family_or_friends: "false",
+      all_citizens: "true",
+    }
+  }
 
-      expect(last_response).to be_ok
-      expect(response_json.keys).to eq [ "household_members", "other_documents_needed" ]
-    end
+  let(:path) {
+    "/api?#{default_params.to_query}"
+  }
+
+  it "returns data" do
+    get path, format: :json
+
+    expect(last_response).to be_ok
+    expect(response_json.keys).to eq  [
+      "residency_documents", "identity_documents", "citizenship_documents", "income_documents"
+    ]
   end
 
 end
