9380fc7312394a37671f33a25944dfbe06a9ddb9
Alex Soble <asoble@gmail.com>
2016-08-03 14:40:04 -0500

    Start a new DocumentsApi spec from scratch

--- spec/documents_api_spec.rb
+++ spec/documents_api_spec.rb
@@ -2,138 +2,46 @@ require_relative "../documents_api"
 
 describe Api::DocumentsRequest do
 
-  subject {
-    described_class.new(
-      household_members: incoming_params[:household_members],
-      has_rental_income: incoming_params[:has_rental_income]
-    )
+  subject { described_class.new(params) }
+
+  let(:default_params) {
+    {
+      disability_benefits: "false",
+      employee: "false",
+      self_employed: "false",
+      child_support: "false",
+      retired: "false",
+      unemployment_benefits: "false",
+      has_rental_income: "false",
+      renting: "false",
+      owns_home: "false",
+      shelter: "false",
+      living_with_family_or_friends: "false",
+      all_citizens: "true",
+    }
   }
 
-  let(:outcome) { subject.fetch_documents }
+  context "default params from screener" do
+    let(:params) { default_params }
 
-  describe "a one-person household" do
-
-    describe "person is an employee" do
-
-      let(:incoming_params) { SINGLE_HOUSEHOLD_MEMBER_EMPLOYED }
-
-      it "should require paystubs documentation" do
-
-        expect(document_names.size).to eq 1
-        expect(document_names).to eq ["Pay Stubs"]
-        expect(outcome[:other_documents_needed]).to eq [subject.residency_documents]
-      end
-
+    it "returns the correct documents data" do
+      expect(subject.fetch_documents).to eq({
+        residency_documents: [
+          { official_name: "State ID", description: "State ID"},
+          { official_name: "Mail", description: "Mail document showing postmark within last 30 days with Illinois address"},
+          { official_name: "Medical Records", description: "Medical records/clinic cards"}
+        ],
+        identity_documents: [
+          { official_name: "State ID", description: "State ID"},
+          { official_name: "School Photo ID"},
+          { official_name: "U.S. Military ID Card"},
+          { official_name: "Voter Registration Card"},
+          { official_name: "Birth Certificate"}
+        ],
+        citizenship_documents: [],
+        income_documents: [],
+      })
     end
-
-    describe "person is self-employed" do
-
-      let(:benefits_application) { SINGLE_HOUSEHOLD_MEMBER_SELF_EMPLOYED }
-
-      it "should require self-employment form" do
-
-        expect(document_names.size).to eq 1
-        expect(document_names).to eq ["Self-Employment Form"]
-
-      end
-
-    end
-
-    describe "applying for expedited benefits" do
-      let(:benefits_application) { EXPEDITED_BENEFITS }
-      let(:other_documents_needed) { outcome[:other_documents_needed] }
-
-      it "should require bank statements" do
-        expect(other_documents_needed.size).to eq 2
-        expect(other_documents_needed).to eq [
-          subject.residency_documents, BANK_STATEMENTS
-        ]
-      end
-
-    end
-
-    describe "has rental income" do
-      let(:benefits_application) { SINGLE_HOUSEHOLD_MEMBER_WITH_RENTAL_INCOME }
-      let(:other_documents_needed) { outcome[:other_documents_needed] }
-
-      it "should require bank statements" do
-        expect(other_documents_needed.size).to eq 2
-        expect(other_documents_needed).to eq [
-          subject.residency_documents, BANK_STATEMENTS
-        ]
-      end
-    end
-
-  end
-
-  describe "a multi-person household" do
-
-    let(:applicant) { outcome[:household_members][0] }
-    let(:documents) { applicant[:documents_needed] }
-    let(:document_names) { documents.map { |document| document[:official_name] } }
-
-    let(:document_names_per_person) {
-      outcome[:household_members].map do |household_member|
-        household_member[:documents_needed].map do |document|
-          document[:official_name]
-        end
-      end
-    }
-
-    describe "head of household working and receiving child support" do
-
-      let(:benefits_application) { MULTI_MEMBER_HOUSEHOLD_RECEIVING_CHILD_SUPPORT }
-
-      it "returns the proper documents:, pay stubs, and written child support statement" do
-
-        expect(documents.size).to eq 2
-        expect(document_names).to eq ["Pay Stubs", "Written Child Support Statement"]
-
-      end
-
-    end
-
-    describe "multi-member household with 1 retired person, 1 employee," \
-             "and 1 disabled person" do
-
-      let(:benefits_application) {
-        MULTI_MEMBER_HOUSEHOLD_WITH_RETIREE_DISABLED_AND_WORKING
-      }
-
-      it "returns the proper documents for each person:" \
-         "award letter for retired person, award letter for disabled person," \
-         "pay stubs for employee" do
-
-        expect(document_names_per_person).to eq [
-          ["Award Letter from Social Security"],
-          ["Award Letter for Disability"],
-          ["Pay Stubs"]
-        ]
-
-      end
-
-    end
-
-    describe "multi-member household with 1 person receiving unemployment" \
-             "and 1 person working" do
-
-      let(:benefits_application) {
-        MULTI_MEMBER_HOUSEHOLD_WITH_UNEMPLOYED_AND_WORKING
-      }
-
-      it "returns the proper documents:" \
-         "employee should submit pay stubs," \
-         "unemployed person should submit award letter for unemployment," do
-
-        expect(document_names_per_person).to eq [
-          ["Award Letter for Unemployment"],
-          ["Pay Stubs"]
-        ]
-
-      end
-
-    end
-
   end
 
 end
