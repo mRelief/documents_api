edc4ab1bc7d711e1a274dbcbbf11bd4f27d1fd51
Alex Soble <asoble@gmail.com>
2016-08-15 12:34:57 -0500

    Move SMS screener questions into their own file

--- incoming_message_handler.rb
+++ incoming_message_handler.rb
@@ -1,31 +1,16 @@
+require_relative 'sms_screener_questions'
+
 class IncomingMessageHandler < Struct.new :from, :body, :step
 
   def client
     Twilio::REST::Client.new(ENV['account_sid'], ENV['auth_token'])
   end
 
-  def welcome_message
-    'Welcome. Here you can find out what documents you need to apply for Food Stamps. '
-  end
-
-  def first_question
-    'How many people are you applying for? A. Just Me. B. My Family.'
-  end
-
-  def housing_question
-    'Describe your living situation: ' +
-    'A. Renting' +
-    'B. Own home' +
-    'C. Living with family/friends' +
-    'D. Shelter' +
-    'E. None of the above'
-  end
-
   def message
     if step == "initial"
-      welcome_message + first_question
+      SMS_SCREENER['welcome_message'] + SMS_SCREENER['first_question']
     elsif step == "housing"
-      housing_question
+      SMS_SCREENER['housing_question']
     end
   end
 
---
+++ sms_screener_questions.rb
@@ -0,0 +1,12 @@
+SMS_SCREENER = {
+  'welcome_message' => 'Welcome. Here you can find out what documents you need to apply for Food Stamps. ',
+  'first_question' => 'How many people are you applying for? A. Just Me. B. My Family.',
+  'housing_question' => (
+    'Describe your living situation: ' +
+    'A. Renting' +
+    'B. Own home' +
+    'C. Living with family/friends' +
+    'D. Shelter' +
+    'E. None of the above'
+  )
+}
