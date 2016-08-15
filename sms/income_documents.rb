af08781af098b1a9155682a98b7fe1439419cafe
Alex Soble <asoble@gmail.com>
2016-08-15 15:43:34 -0500

    Use the right variable name in IncomeDocuments

--- sms/income_documents.rb
+++ sms/income_documents.rb
@@ -15,31 +15,31 @@ class IncomeDocuments < Struct.new :unwrapped_session
   private
 
   def documents_based_on_employment
-    'Pay Stubs' if session.employee?
+    'Pay Stubs' if unwrapped_session.employee?
   end
 
   def documents_based_on_self_employment
-    'Self-Employment Form' if session.self_employed?
+    'Self-Employment Form' if unwrapped_session.self_employed?
   end
 
   def documents_based_on_child_support
-    'Written Child Support Statement' if session.child_support?
+    'Written Child Support Statement' if unwrapped_session.child_support?
   end
 
   def documents_based_on_disability
-    'Award Letter for Disability' if session.disability_benefits?
+    'Award Letter for Disability' if unwrapped_session.disability_benefits?
   end
 
   def documents_based_on_retirement
-    'Award Letter from Social Security' if session.retired?
+    'Award Letter from Social Security' if unwrapped_session.retired?
   end
 
   def documents_based_on_unemployment
-    'Award Letter for Unemployment' if session.unemployment_benefits?
+    'Award Letter for Unemployment' if unwrapped_session.unemployment_benefits?
   end
 
   def documents_based_on_rental_income
-    'Bank Statements' if session.has_rental_income?
+    'Bank Statements' if unwrapped_session.has_rental_income?
   end
 
 end
