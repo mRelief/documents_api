533b6358b3b7a3d2e4d83a5a8c937c43f636db3b
Alex Soble <asoble@gmail.com>
2016-08-16 16:06:00 -0500

    Organize spec into state ID/no state ID

--- spec/sms/sms_integration_spec.rb
+++ spec/sms/sms_integration_spec.rb
@@ -15,92 +15,100 @@ describe 'SMS conversation' do
     }
   end
 
-  describe '1 person, renting, citizen, self-employed, no other income, has state ID' do
-    let(:expected_documents) {
-      'You will need these documents: State ID, Self-Employment Form.'
-    }
+  describe 'has State ID' do
 
-    it 'responds with the correct documents' do
-      send_sms('Hi!')
-      send_sms('A')    # Just Me
-      send_sms('A')    # Renting
-      send_sms('YES')  # All citizens
-      send_sms('B')    # Self-employed
-      send_sms('D')    # None of the above
-      send_sms('YES')  # Has State ID
-      expect(last_response.body).to eq expected_documents
+    describe '1 person, renting, citizen, self-employed, no other income, has state ID' do
+      let(:expected_documents) {
+        'You will need these documents: State ID, Self-Employment Form.'
+      }
+
+      it 'responds with the correct documents' do
+        send_sms('Hi!')
+        send_sms('A')    # Just Me
+        send_sms('A')    # Renting
+        send_sms('YES')  # All citizens
+        send_sms('B')    # Self-employed
+        send_sms('D')    # None of the above
+        send_sms('YES')  # Has State ID
+        expect(last_response.body).to eq expected_documents
+      end
     end
+
+    describe '1 person, renting, citizen, employee, no other income, has state ID' do
+      let(:expected_documents) {
+        'You will need these documents: State ID, Pay Stubs.'
+      }
+
+      it 'responds with the correct documents' do
+        send_sms('Hi!')
+        send_sms('A')    # Just Me
+        send_sms('A')    # Renting
+        send_sms('YES')  # All citizens
+        send_sms('A')    # Employee
+        send_sms('D')    # None of the above
+        send_sms('YES')  # Has State ID
+        expect(last_response.body).to eq expected_documents
+      end
+    end
+
+    describe '1 person, owns home, citizen, employee, child support, has state ID' do
+      let(:expected_documents) {
+        'You will need these documents: ' +
+        'State ID, Pay Stubs, Written Child Support Statement.'
+      }
+
+      it 'responds with the correct documents' do
+        send_sms('Hi!')
+        send_sms('A')    # Just Me
+        send_sms('B')    # Owns home
+        send_sms('YES')  # All citizens
+        send_sms('A')    # Employee
+        send_sms('B')    # Child support
+        send_sms('YES')  # Has State ID
+        expect(last_response.body).to eq expected_documents
+      end
+    end
+
   end
 
-  describe 'family, renting, citizens, self-employed, no other income, has state ID' do
-    let(:expected_documents) {
-      'You will need these documents: ' +
-      'State IDs for everyone you are applying for, Self-Employment Form.'
-    }
+  describe 'no state ID' do
 
-    it 'responds with the correct documents' do
-      send_sms('Hi!')
-      send_sms('B')    # My Family
-      send_sms('A')    # Renting
-      send_sms('YES')  # All citizens
-      send_sms('B')    # Self-employed
-      send_sms('D')    # None of the above
-      send_sms('NO')   # No State ID
-      expect(last_response.body).to eq expected_documents
+    describe '1 person, renting, not citizen, self-employed, no other income, no state ID' do
+      let(:expected_documents) {
+        'You will need these documents: ' +
+        'State ID, I-90 Documentation for all non-citizen family members, Self-Employment Form.'
+      }
+
+      it 'responds with the correct documents' do
+        send_sms('Hi!')
+        send_sms('A')    # 1 Person
+        send_sms('A')    # Renting
+        send_sms('NO')   # Not citizen
+        send_sms('B')    # Self-employed
+        send_sms('D')    # None of the above
+        send_sms('NO')   # No State ID
+        expect(last_response.body).to eq expected_documents
+      end
     end
-  end
 
-  describe '1 person, renting, not citizen, self-employed, no other income, has state ID' do
-    let(:expected_documents) {
-      'You will need these documents: ' +
-      'State ID, I-90 Documentation for all non-citizen family members, Self-Employment Form.'
-    }
+    describe 'family, renting, citizens, self-employed, no other income, has state ID' do
+      let(:expected_documents) {
+        'You will need these documents: ' +
+        'State IDs for everyone you are applying for, Self-Employment Form.'
+      }
 
-    it 'responds with the correct documents' do
-      send_sms('Hi!')
-      send_sms('A')    # 1 Person
-      send_sms('A')    # Renting
-      send_sms('NO')   # Not citizen
-      send_sms('B')    # Self-employed
-      send_sms('D')    # None of the above
-      send_sms('NO')   # No State ID
-      expect(last_response.body).to eq expected_documents
+      it 'responds with the correct documents' do
+        send_sms('Hi!')
+        send_sms('B')    # My Family
+        send_sms('A')    # Renting
+        send_sms('YES')  # All citizens
+        send_sms('B')    # Self-employed
+        send_sms('D')    # None of the above
+        send_sms('NO')   # No State ID
+        expect(last_response.body).to eq expected_documents
+      end
     end
-  end
 
-  describe '1 person, renting, citizen, employee, no other income, has state ID' do
-    let(:expected_documents) {
-      'You will need these documents: State ID, Pay Stubs.'
-    }
-
-    it 'responds with the correct documents' do
-      send_sms('Hi!')
-      send_sms('A')    # Just Me
-      send_sms('A')    # Renting
-      send_sms('YES')  # All citizens
-      send_sms('A')    # Employee
-      send_sms('D')    # None of the above
-      send_sms('YES')  # Has State ID
-      expect(last_response.body).to eq expected_documents
-    end
-  end
-
-  describe '1 person, owns home, citizen, employee, child support, has state ID' do
-    let(:expected_documents) {
-      'You will need these documents: ' +
-      'State ID, Pay Stubs, Written Child Support Statement.'
-    }
-
-    it 'responds with the correct documents' do
-      send_sms('Hi!')
-      send_sms('A')    # Just Me
-      send_sms('B')    # Owns home
-      send_sms('YES')  # All citizens
-      send_sms('A')    # Employee
-      send_sms('B')    # Child support
-      send_sms('YES')  # Has State ID
-      expect(last_response.body).to eq expected_documents
-    end
   end
 
 end
