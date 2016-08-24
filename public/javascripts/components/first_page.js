94fb9615879216aee030116027d3319b9cb52df0
Alex Soble <asoble@gmail.com>
2016-08-24 18:06:10 -0400

    Make both questions required on first page
    + Validate #151

--- public/javascripts/components/first_page.js
+++ public/javascripts/components/first_page.js
@@ -17,16 +17,28 @@
       userSubmittedData: React.PropTypes.func.isRequired
     },
 
+    getInitialState: function () {
+      return {
+        showPleaseAnswerNumberOfPeople: false,
+        showPleaseAnswerHousing: false
+      };
+    },
+
     render: function () {
+      var showNumberOfPeopleWarning = this.state.showPleaseAnswerNumberOfPeople;
+      var showHosuingQuestionWarning = this.state.showPleaseAnswerHousing;
+
       return dom.div({},
         this.renderNumberOfPeople(),
+        this.requiredQuestionWarning(showNumberOfPeopleWarning),
         this.renderHousingQuestion(),
+        this.requiredQuestionWarning(showHosuingQuestionWarning),
         dom.br({}),
         dom.input({
           type: 'submit',
           value: 'Next',
           style: window.shared.ButtonStyle,
-          onClick: this.props.onClickNext
+          onClick: this.onClickNext
         })
       );
     },
@@ -45,6 +57,61 @@
         userWentBack: this.props.userWentBack,
         userSubmittedData: this.props.userSubmittedData,
       });
+    },
+
+    onClickNext: function () {
+      if (this.bothQuestionsAnswered()) {
+        this.props.onClickNext();
+      } else {
+        this.validateFirstQuestionAnswered();
+        this.validateSecondQuestionAnswered();
+      };
+    },
+
+    validateFirstQuestionAnswered: function () {
+      if (!this.numberOfPeopleQuestionAnswered()) {
+        this.setState({ showPleaseAnswerNumberOfPeople: true });
+      } else {
+        this.setState({ showPleaseAnswerNumberOfPeople: false });
+      };
+    },
+
+    validateSecondQuestionAnswered: function () {
+      if (!this.housingQuestionAnswered()) {
+        this.setState({ showPleaseAnswerHousing: true });
+      } else {
+        this.setState({ showPleaseAnswerHousing: false });
+      };
+    },
+
+    bothQuestionsAnswered: function () {
+      return (this.numberOfPeopleQuestionAnswered() && this.housingQuestionAnswered());
+    },
+
+    numberOfPeopleQuestionAnswered: function () {
+      return $('[type="radio"][name="NumberOfPeopleQuestion"]').get().map(function(checkbox) {
+        return checkbox.checked;
+      }).reduce(function(a, b) { return (a || b); });
+    },
+
+    housingQuestionAnswered: function () {
+      return $('[type="radio"][name="livingQuestion"]').get().map(function(checkbox) {
+        return checkbox.checked;
+      }).reduce(function(a, b) { return (a || b); });
+    },
+
+    requiredQuestionWarning: function (shouldShow) {
+      if (!shouldShow) return null;
+
+      return dom.div({},
+        dom.div({
+          style: {
+            color: 'red',
+            fontStyle: 'italic'
+          }
+        }, 'Please select a response.'),
+        dom.br({})
+      );
     },
 
   });
