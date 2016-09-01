17ecdbb210b5309690d99d3b2446525929481cad
Alex Soble <asoble@gmail.com>
2016-09-01 18:17:18 -0500

    Strip out periods from incoming SMS messages
    + Fix #205

--- sms/incoming_message_cleaner.rb
+++ sms/incoming_message_cleaner.rb
@@ -1,7 +1,9 @@
 class IncomingMessageCleaner < Struct.new :original_message_body
 
   def cleaned
-    original_message_body.split('-')[0].upcase.strip
+    original_message_body.split('-')[0].upcase
+                                       .gsub('.', '')
+                                       .strip
   end
 
 end
