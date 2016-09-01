e3fde94d8df7646f00b12f06266e1b7644dc6c6e
Alex Soble <asoble@gmail.com>
2016-09-01 08:03:56 -0500

    Pull out YES/NO and multiple choice into separate methods

--- sms/response_validator.rb
+++ sms/response_validator.rb
@@ -3,6 +3,12 @@ class ResponseValidator < Struct.new :from, :original_body, :session
   def valid?
     return true if body == 'OPTIONS'
     return true if body == 'RESET'
+    return true if valid_for_multiple_choice?
+    return true if valid_for_yes_no?
+    return false
+  end
+
+  def valid_for_multiple_choice?
     return true if body.include? 'A'
     return true if body.include? 'B'
     return true if body.include? 'C'
@@ -10,11 +16,14 @@ class ResponseValidator < Struct.new :from, :original_body, :session
     return true if body == 'E'
     return true if body == 'F'
     return true if body == 'G'
+    return false
+  end
+
+  def valid_for_yes_no?
     return true if body == 'Y'
     return true if body == 'YES'
     return true if body == 'N'
     return true if body == 'NO'
-    return false
   end
 
   def respond_to_invalid!
