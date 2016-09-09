edb7123f9314434f56e1148064b1e0feb009c3d5
Alex Soble <asoble@gmail.com>
2016-09-09 14:18:08 -0500

    Add instructions after letter-based questions
    + Fix #213.

--- sms/sms_screener_questions.rb
+++ sms/sms_screener_questions.rb
@@ -3,7 +3,8 @@ SMS_SCREENER = {
     'Welcome. ' +
     'Here you can find out what documents you need to apply for Food Stamps. ' +
     'How many people are you applying for? ' +
-    'A. Just Me. B. Me and My Family.'
+    'A. Just Me. B. Me and My Family. ' +
+    'Please enter A or B.'
   ),
   1 => (
     'Describe your living situation: ' +
@@ -11,6 +12,7 @@ SMS_SCREENER = {
     'B. Own home. ' +
     'C. Living with family/friends. ' +
     'D. Shelter. ' +
+    'Please enter A, B, C, or D. ' +
     'For more options, type \'options.\''
   ),
   2 => (
@@ -23,14 +25,16 @@ SMS_SCREENER = {
     'B. Self-employed. ' +
     'C. Retired. ' +
     'D. Receiving unemployment benefits. ' +
-    'E. None of the above.'
+    'E. None of the above. ' +
+    'Please enter A, B, C, D, E, or a combination. For example: AB.'
   ),
   4 => (
     'Which of the following do you receive: ' +
     'A. Disability benefits. ' +
     'B. Child support. ' +
     'C. Rental income. ' +
-    'D. None of the above.'
+    'D. None of the above. ' +
+    'Please enter A, B, C, D, or a combination. For example: BC.'
   ),
   5 => (
     'Do you have a State ID? Y or N.'
