23976f44aa062393b2b36d657f03305d5ccadf88
Alex Soble <asoble@gmail.com>
2016-09-12 15:42:33 -0500

    Add more tiered question control variables to our session object

--- app.rb
+++ app.rb
@@ -73,8 +73,11 @@ post '/sms' do
   session['disability_benefits'] ||= 'false'
   session['child_support'] ||= 'false'
   session['has_state_id'] ||= 'true'
-  session['more_housing_options'] ||= 'false'
   session['recently_lost_job_and_received_paycheck'] ||= 'false'
+
+  session['more_housing_options'] ||= 'false'
+  session['tiered_unemployment_question_one'] ||= 'false'
+  session['tiered_unemployment_question_two'] ||= 'false'
 
   body = IncomingMessageCleaner.new(params[:Body]).cleaned
 
